7318b309df0a6858a619ef627f58e70d554616bf
hilalbsr <hilalbusrabodur@gmail.com>
2023-10-04 21:59:15 +0300

    in-memory caching

--- InMemoryApp.Web/Controllers/ProductController.cs
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InMemoryApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace InMemoryApp.Web.Controllers
@@ -14,50 +15,74 @@ namespace InMemoryApp.Web.Controllers
 
         public ActionResult Index()
         {
-            //memory de data var mı?
+            #region SimpleCaching
+            ////memory de data var mı?
 
-            //1.YOL
-            //if (string.IsNullOrWhiteSpace(_memoryCache.Get<string>("zaman")))
+            ////1.YOL
+            ////if (string.IsNullOrWhiteSpace(_memoryCache.Get<string>("zaman")))
+            ////{
+            ////    //memorye tarih bilgisi cacheleme
+            ////    _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            ////}
+
+            ////if (!_memoryCache.TryGetValue("zaman", out string zamancache))
+            ////{
+            ////    //memorye tarih bilgisi cacheleme
+            ////    _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            ////    var degsken = zamancache;
+            ////}
+
+            ////ömür bilgisi
+            //MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+            ////options.AbsoluteExpiration = DateTime.Now.AddSeconds(10);
+
+            //options.AbsoluteExpiration = DateTime.Now.AddSeconds(1);
+            //options.SlidingExpiration = TimeSpan.FromSeconds(10);
+            //options.Priority = CacheItemPriority.NeverRemove;
+
+            //options.RegisterPostEvictionCallback((key, value, reason, state) =>
             //{
-            //    //memorye tarih bilgisi cacheleme
-            //    _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
-            //}
+            //    //silinecek olan = key
+            //    //değeri = value
+            //    //silinme sebebi = reason
+            //    _memoryCache.Set("callback", $"{key} -> {value} => sebep : {reason}");
+            //});
 
-            //if (!_memoryCache.TryGetValue("zaman", out string zamancache))
-            //{
-            //    //memorye tarih bilgisi cacheleme
-            //    _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
-            //    var degsken = zamancache;
-            //}
+            ////memorye tarih bilgisi cacheleme
+            //_memoryCache.Set<string>("zaman", DateTime.Now.ToString(), options);
 
-            //ömür bilgisi
-            MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
-            //options.AbsoluteExpiration = DateTime.Now.AddSeconds(10);
+            #endregion
 
-            options.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
-            options.SlidingExpiration = TimeSpan.FromSeconds(10);
+            Product product = new Product { Id = 1, Name = "Kalem", Price = 200 };
 
-            //memorye tarih bilgisi cacheleme
-            _memoryCache.Set<string>("zaman", DateTime.Now.ToString(), options);
-
+            _memoryCache.Set<Product>("product:1", product);
 
             return View();
         }
 
         public IActionResult Show()
         {
-            //bu key'a ait değeri almaya çalışır. Yok ise oluşturur.
-            //var olup olmadığını kontrol etmeye gerek kalmıyor.
-            //_memoryCache.GetOrCreate<string>("zaman", entry =>
-            //{
-            //    return DateTime.Now.ToString();
-            //});
+            #region SimpleCaching
+            ////bu key'a ait değeri almaya çalışır. Yok ise oluşturur.
+            ////var olup olmadığını kontrol etmeye gerek kalmıyor.
+            ////_memoryCache.GetOrCreate<string>("zaman", entry =>
+            ////{
+            ////    return DateTime.Now.ToString();
+            ////});
 
-            _memoryCache.TryGetValue("zaman", out string zamancache);
-            ViewBag.zaman = zamancache;
+            //_memoryCache.TryGetValue("zaman", out string zamancache);
+            //_memoryCache.TryGetValue("callback", out string callback);
 
-            //tarih memorydan data çekme
-            //ViewBag.zaman = _memoryCache.Get<string>("zaman");
+            //ViewBag.zaman = zamancache;
+            //ViewBag.callback = callback;
+
+
+            ////tarih memorydan data çekme
+            ////ViewBag.zaman = _memoryCache.Get<string>("zaman");
+            #endregion
+
+            ViewBag.product = _memoryCache.Get<Product>("product:1");
+
             return View();
         }
     }
